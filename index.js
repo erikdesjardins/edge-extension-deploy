d67d5ec99569e8a6c851a24cf9a73c114d45543b
Erik <erikdesjardins@users.noreply.github.com>
2017-02-15 18:39:01 -0500

    update the submission so that it pulls the new package from the uploaded zip

--- index.js
+++ index.js
@@ -130,6 +130,32 @@ module.exports = function deploy(options) {
 					throw new Error('Failed to upload package: ' + (err.response.body.code || err.response.status));
 				});
 		})
+		// update the submission to intake the new package
+		.then(function() {
+			// https://docs.microsoft.com/en-us/windows/uwp/monetize/update-an-app-submission
+			// https://docs.microsoft.com/en-us/windows/uwp/monetize/update-a-flight-submission
+			return request
+				.put(appAndFlight + '/submissions/' + submissionInfo.id)
+				.set('Authorization', 'Bearer ' + accessToken)
+				.send(Object.assign({}, submissionInfo, {
+					[flightId ? 'flightPackages' : 'applicationPackages']: [{
+						fileName: 'package.appx',
+						fileStatus: 'PendingUpload',
+						minimumDirectXVersion: 'None',
+						minimumSystemRam: 'None'
+					}].concat(submissionInfo[flightId ? 'flightPackages' : 'applicationPackages'].map(function(pack) {
+						// remove old packages
+						return Object.assign({}, pack, {
+							fileStatus: 'PendingDelete'
+						});
+					}))
+				}))
+				.then(function(response) {
+					submissionInfo = response.body;
+				}, function(err) {
+					throw new Error('Failed to update submission: ' + (err.response.body.code || err.response.status));
+				});
+		})
 		// commit new submission
 		.then(function() {
 			// https://docs.microsoft.com/en-us/windows/uwp/monetize/commit-an-app-submission
